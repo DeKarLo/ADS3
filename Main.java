2ae45e46a45273a074a83c835c3988c8ed8e06ec
Dek <karen.ananyan2017@mail.ru>
2023-05-01 22:59:52 +0600

    Main test class

--- Main.java
+++ Main.java
@@ -1,3 +1,21 @@
+import java.util.Random;
+
 public class Main {
+    public static void main(String[] args) {
+        MyHashTable<MyTestingClass, Student> table = new MyHashTable<>();
+
+        // Add 10000 random elements to the hashtable
+        Random random = new Random();
+        for (int i = 0; i < 10000; i++) {
+            MyTestingClass key = new MyTestingClass(i, "Name" + i);
+            Student value = new Student("Student" + i, random.nextInt(100));
+            table.put(key, value);
+        }
+        System.out.println(table);
+        for (int i = 0; i < table.getM(); i++) {
+            System.out.println("Bucket " + i + ": " + table.getBucketSize(i));
+        }
+
+    }
 
 }
