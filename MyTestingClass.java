f337104b864b6b255ed1930d2cf8e3bd706a0660
Dek <karen.ananyan2017@mail.ru>
2023-05-01 22:27:10 +0600

    MyTestingClass create hashCode()

--- MyTestingClass.java
+++ MyTestingClass.java
@@ -19,7 +19,12 @@ class MyTestingClass {
     // Custom hashCode method that generates a random number based on the id and name
     @Override
     public int hashCode() {
-        return 0;
+        int hash = 23;
+        hash = hash*31+this.id;
+        for (int i = 0; i < name.length(); i++){
+            hash += (int) name.charAt(i)*31;
+        }
+        return hash;
     }
 
     // Custom equals method that compares the id and name fields
